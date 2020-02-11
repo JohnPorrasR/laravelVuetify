827684b5227a8803a1fcd592070cd5cdf0dee454
John Porras R <johnporrasr@gmail.com>
2020-02-11 17:43:42 -0500

    Personalizando el paginador del Api

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -49,15 +49,15 @@ trait ApiResponser
 
     protected function paginate(Collection $collection)
     {
-        /*$rules = [
+        $rules = [
             'per_page' => 'integer|min:2|max:50'
         ];
-        Validator::validate(request()->all(), $rules);*/
+        Validator::validate(request()->all(), $rules);
         $page = LengthAwarePaginator::resolveCurrentPage();
         $perPage = 5;
-        /*if (request()->has('per_page')) {
+        if (request()->has('per_page')) {
             $perPage = (int) request()->per_page;
-        }*/
+        }
         $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
         $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
             'path' => LengthAwarePaginator::resolveCurrentPath(),
